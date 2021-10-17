8ddbd3dc0744832036b19c42a2174f1128e71329
Cheuk Hei Gordon Cheung <cheungcheukheigordon@gmail.com>
2021-10-17 16:11:46 +0100

    TradeQ

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_bcrypt import Bcrypt
+from forms import SignUpForm, LoginForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SECRET_KEY'] = '188db0c1a734f4d4d31f26f6c0ef5562d7aa4910caeb09b2bd402a25edba2d51b4000e2245f818c9e1216cb62e3e98761ebbaac40510ce4608213ef327e32e12322f02e423'
 db = SQLAlchemy(app)
 
 class Messages(db.Model):
@@ -18,12 +21,26 @@ class Messages(db.Model):
 def index():
     return render_template('index.html')
 
-@app.route('/feedback')
+@app.route('/support')
 def feedback():
     if request.method == 'POST':
-        return redirect(url_for('feedback'))
-    
-    return render_template('feedback.html')
+        return redirect(url_for('support'))
+    return render_template('support.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f"Account successfully created for {form.username.data}", "success")
+        return redirect(url_for('index'))
+    return render_template('sign_up.html', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Successfully logged in as {form.username.data}", "success")
+    return render_template('login.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
