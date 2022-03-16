09bbc287906205d7f4e7c6aa10682fc9dab0bfe2
Lemon0307 <cheungcheukheigordon@gmail.com>
2022-03-16 20:05:56 +0000

    MucQ

--- mucq/static/js/main.js
+++ mucq/static/js/main.js
@@ -23,6 +23,8 @@ var language = {
         content: 'Content',
         create: 'Create',
         mucq_title: 'Welcome to MucQ!',
+        login: 'Login',
+        sign_up: 'Sign Up',
         intro: 'Welcome to MucQ! See what/s new!'
     },
     chi: {
@@ -31,6 +33,8 @@ var language = {
         title: '題目',
         content: '內容',
         create: '創造',
+        login: '登入',
+        sign_up: '登記',
         mucq_title: '歡迎光臨到 MucQ!',
         intro: '做 MucQ，講 MucQ，MucQ 都喺晒度！'
     },
@@ -39,6 +43,8 @@ var language = {
 
 if (window.location.hash) {
     if (window.location.hash === '#chi') {
+      login.textContent = language.chi.login;
+        sign_upContent = language.chi.sign_up;
         home.textContent = language.chi.home;
         menu_home.textContent = language.chi.home;
         menu_support.textContent = language.chi.support;
@@ -48,6 +54,8 @@ if (window.location.hash) {
         create_post_content.textContent = language.chi.content;
     };
     if (window.location.hash === '#eng') {
+      login.textContent = language.eng.login;
+        sign_upContent = language.eng.sign_up;
         home.textContent = language.eng.home;
         menu_home.textContent = language.eng.home;
         menu_support.textContent = language.eng.support;
