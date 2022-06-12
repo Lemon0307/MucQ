b992309f26ea832cc45d1fba3c08c4c143c14ea4
Lemon0307 <cheungcheukheigordon@gmail.com>
2022-06-12 12:57:09 +0100

    MucQ

--- mucq/products_folder/routes.py
+++ mucq/products_folder/routes.py
@@ -5,7 +5,19 @@ from flask_login import current_user
 
 products = Blueprint('products', __name__)
 
-@products.route('/products', methods=['GET', 'POST'])
+@products.route('/products')
 
 def product():
     return render_template('products/products.html')
+
+@products.route('/products/<int:product_id>', methods=['GET', 'POST'])
+
+def product_view(product_id):
+    import mucq.models
+    product = mucq.models.Products.query.get_or_404(product_id)
+    return render_template('/products/product_view.html', product=product)
+
+@products.route('/create_product', methods=['GET', 'POST'])
+
+def create_product():
+    pass
